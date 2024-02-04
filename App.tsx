56034c1fbe3fed34760359d638337d2212370459
NelsonFrank <nelsonfr78@outlook.com>
2024-02-04 16:52:47 +0300

    feat(ui): :sparkles: add button component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import { Button } from './src/components/ui/Button';
+import "./src/styles/global.css";
 
 export default function App() {
   return (
     <View className='flex-1 justify-center items-center'>
       <Text className='text-3xl text-blue-500'>Open up your app!</Text>
-      <StatusBar style="auto" />
+      <Button label='Add Color' variant="destructive"></Button>
+      <StatusBar style="light" />
     </View>
   );
 }
