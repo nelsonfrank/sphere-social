b32e6096a318981c5ec797dccf3835a94fe25915
NelsonFrank <nelsonfr78@outlook.com>
2024-02-04 23:48:58 +0300

    feat: :sparkles: add app routes

--- App.tsx
+++ App.tsx
@@ -1,15 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import { Button } from './src/components/ui/Button';
 import "./src/styles/global.css";
+import AppRoutes from "@/app.routes";
 
 export default function App() {
-  return (
-    <View className='flex-1 justify-center items-center'>
-      <Text className='text-3xl text-blue-500'>Open up your app!</Text>
-      <Button label='Add Color' variant="destructive"></Button>
-      <StatusBar style="light" />
-    </View>
-  );
+  return <AppRoutes />;
 }
-
---
+++ src/app.routes.tsx
@@ -0,0 +1,15 @@
+import WelcomeScreen from "./screens/welcome";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Stack = createNativeStackNavigator();
+
+export default function AppRoutes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
