c53634288030b1e1cb98163cae714155db9e3de9
NelsonFrank <nelsonfr78@outlook.com>
2024-02-13 02:04:12 +0300

    feat: :sparkles: add home screen

--- src/app.routes.tsx
+++ src/app.routes.tsx
@@ -1,6 +1,10 @@
-import WelcomeScreen from "./screens/welcome";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
+
+// Screens
+import CategoriesSelection from "./screens/caterogies-selection";
+import WelcomeScreen from "./screens/welcome";
+import HomeScreen from './screens/home';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,8 +15,11 @@ export default function AppRoutes() {
         screenOptions={() => ({
           headerShown: false,
         })}
+        initialRouteName="Welcome"
       >
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="Categories" component={CategoriesSelection} />
+        <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
